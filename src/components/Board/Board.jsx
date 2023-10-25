555bc8022745a7969daab2b508f1b0c55ef71e69
ivyzhang <xiaowen_zhang@outlook.com>
2023-10-26 00:09:34 +0800

    v1.0 setup project skeleton

---
+++ src/components/Board/Board.jsx
@@ -0,0 +1,17 @@
+import "./Board.css";
+
+export default function Board() {
+  return (
+    <main id="board">
+      <div id="0">Test</div>
+      <div id="1">Test</div>
+      <div id="2">Test</div>
+      <div id="3">Test</div>
+      <div id="4">Test</div>
+      <div id="5">Test</div>
+      <div id="6">Test</div>
+      <div id="7">Test</div>
+      <div id="8">Test</div>
+    </main>
+  );
+}
