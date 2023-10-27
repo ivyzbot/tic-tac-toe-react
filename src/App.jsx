11fc51a1a676915202c178e28e171e44201a9c9f
ivyzhang <xiaowen_zhang@outlook.com>
2023-10-28 00:36:33 +0800

    v1.0 basic functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,68 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Replay from "./components/Replay/Replay";
 import Board from "./components/Board/Board";
 import Turn from "./components/Turn/Turn";
 
+import { color, winningCombos } from "./assets/consts";
 import "./App.css";
 
 function App() {
+  const [squares, setSquares] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+
+  const [turn, setTurn] = useState(1);
+  const [winner, setWinner] = useState(null);
+
+  useEffect(() => {
+    setWinner(checkWinner());
+  }, [squares]);
+
+  function checkWinner() {
+    for (let combo of winningCombos) {
+      if (
+        Math.abs(squares[combo[0]] + squares[combo[1]] + squares[combo[2]]) ===
+        3
+      ) {
+        return squares[combo[0]];
+      }
+    }
+
+    if (squares.includes(null)) {
+      return null;
+    } else {
+      return "T";
+    }
+  }
+
   return (
     <>
-      <Turn />
-
-      <Board />
-      <Replay />
+      <Turn states={{ winner: winner, turn: turn, color: color }} />
+      <Board
+        states={{ winner: winner, squares: squares, turn: turn, color: color }}
+        functions={{
+          checkWinner: checkWinner,
+          setWinner: setWinner,
+          setSquares: setSquares,
+          setTurn: setTurn,
+        }}
+      />
+      <Replay
+        functions={{
+          setTurn: setTurn,
+          setSquares: setSquares,
+          setWinner: setWinner,
+        }}
+      />
     </>
   );
 }
--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,17 +1,39 @@
 import "./Board.css";
+import Sqaure from "../Square/Square";
 
-export default function Board() {
+export default function Board({ states, functions }) {
+  const { squares, turn, winner, color } = states;
+  const { checkWinner, setWinner, setSquares, setTurn } = functions;
+
+  const currentColor = color[turn];
+
+  let boardComponent = [];
+  for (let i = 0; i < 9; i++) {
+    boardComponent.push(
+      <Sqaure
+        id={i}
+        key={i}
+        sqaureColor={color[squares[i]]}
+        taken={squares[i]}
+      />
+    );
+  }
+
+  function clickSqaure(evt) {
+    // console.log(evt.target.id);
+    const idclicked = parseInt(evt.target.id);
+    if (squares[idclicked] || winner) {
+    } else {
+      const sqauresTemp = [...squares];
+      sqauresTemp[idclicked] = turn;
+      setSquares(sqauresTemp);
+      setTurn(-1 * turn);
+      // setWinner(checkWinner());
+    }
+  }
   return (
-    <main id="board">
-      <div id="0">Test</div>
-      <div id="1">Test</div>
-      <div id="2">Test</div>
-      <div id="3">Test</div>
-      <div id="4">Test</div>
-      <div id="5">Test</div>
-      <div id="6">Test</div>
-      <div id="7">Test</div>
-      <div id="8">Test</div>
+    <main id="board" onClick={clickSqaure}>
+      {boardComponent}
     </main>
   );
 }
