2e42b84c59cf710963d0c1ef7aa18a55b98039a7
ivyzhang <xiaowen_zhang@outlook.com>
2023-11-01 23:20:01 +0800

    v1.2 add imgs to represent users

--- src/components/Turn/Turn.jsx
+++ src/components/Turn/Turn.jsx
@@ -6,21 +6,31 @@ export default function Turn({ states }) {
   const currentWinner = color[winner].toUpperCase();
 
   let msg;
+  let player = "";
+  if (turn === 1) {
+    player = "BATMAN";
+  } else if (turn === -1) {
+    player = "JOKER";
+  }
   if (!winner) {
     msg = (
       <h1 id="turn-msg">
-        <span className={currentColor}>{currentColor}</span>
+        <span className={currentColor}>{player}</span>
         {"'s Turn"}
       </h1>
     );
   } else if (winner === "T") {
-    msg = <h1 id="turn-msg">{"It's a tie!!!"}</h1>;
-  } else {
+    msg = <h1 id="turn-msg">{"ANOTHER BATTLE!!!"}</h1>;
+  } else if (winner === 1) {
     msg = (
-      <h1 id="turn-msg">
-        {"Congrates "}
-        <span className={currentWinner}>{currentWinner}</span>
-        {"! You won!"}
+      <h1 id="turn-msg" className="PURPLE">
+        I'm BATMAN!!!
+      </h1>
+    );
+  } else if (winner === -1) {
+    msg = (
+      <h1 id="turn-msg" className="ORANGE">
+        Why so serious?
       </h1>
     );
   }
